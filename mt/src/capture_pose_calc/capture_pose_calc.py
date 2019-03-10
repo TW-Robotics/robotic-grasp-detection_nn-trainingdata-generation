c072cb2a1493f5d36382f8ed3dab66d5223e5d22
JRauer <johannes.rauer@hotmail.com>
2019-03-10 13:58:03 +0100

    Transformation seems now to work correctly

--- mt/src/capture_pose_calc/capture_pose_calc.py
+++ mt/src/capture_pose_calc/capture_pose_calc.py
@@ -46,83 +46,54 @@ def main(args):
 
 	# From v1 to v0 pointing
 	v0 = np.array([0, 0, 0])
-	v1 = np.array([-0.02372714335579641, 0.15962260421743776, 0.6811443656751603])
-	v1 = np.array([0.5, 0.8, 0.7])
-
-	v10 = -v1
-	print v10
-	a = np.linalg.norm(v10)
-	v10 = v10/a
-	print v10
-	print v1
-
-	xyLength = math.sqrt(v10[0]*v10[0] + v10[1]*v10[1])
-	vecLength = math.sqrt(v10[0]*v10[0] + v10[1]*v10[1] + v10[2]*v10[2])
-	print xyLength, vecLength
-	if xyLength == 0:
-		if v10[0] > 0:
-			zAngle = 0#math.pi/2
-		else:
-			zAngle = 0#-math.pi/2
-	else:
-		zAngle = math.acos(v10[1] / xyLength)
+	vi = np.array([0.5, 0.8, 0.7])
 	
-	xAngle = math.acos(xyLength / vecLength)
-	if v10[2] < 0:
-		print "xCorr"
-		xAngle = -xAngle
-	if v10[0] > 0:
-		print "zCorr"
-		zAngle = -zAngle
-
-	print xAngle*180/math.pi, zAngle*180/math.pi
-
-	q = tf.transformations.quaternion_from_euler(xAngle, 0, zAngle, 'rxyz')
-	q = tf.transformations.quaternion_from_euler(-zAngle, 0, -xAngle, 'rzyx')
-	q = tf.transformations.quaternion_from_euler(xAngle, 0, zAngle)
-	#q1 = tf.transformations.quaternion_from_euler(-math.pi/2, 0, 0, 'rxyz')
-	#q = tf.transformations.quaternion_multiply(q, q1)
-
-	print q
-
-	'''q = [0., 0, 0, 0]
-	angle = math.atan2( v10[2], v10[0] )
-	q[0] = v0[0] * math.sin( angle/2)
-	q[1] = v0[1] * math.sin( angle/2)
-	q[2] = v0[2] * math.sin( angle/2)
-	q[3] = math.cos( angle/2 )
-
-	qNorm = math.sqrt(q[0]*q[0] + q[1]*q[1] + q[2]*q[2] + q[3]*q[3])
-	q = [q[0]/qNorm, q[1]/qNorm, q[2]/qNorm, q[3]/qNorm]'''
-
-	# Calculate Quaternions
-	'''a = np.cross(v1, v10)
-	w = math.sqrt(np.linalg.norm(v1, ord=2) * np.linalg.norm(v10, ord=2)) + v1[0]*v10[0] + v1[1]*v10[1] + v1[2]*v10[2]
-	# Normalize Quaternions
-	qNorm = math.sqrt(a[0]*a[0] + a[1]*a[1] + a[2]*a[2] + w*w)
-	#q = [a[0]/qNorm, a[1]/qNorm, a[2]/qNorm, w/qNorm]'''
-	#print q
-
 	# Do at a frequency of 10 Hz
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
-		# Send transformation to tf
-		'''for i in range(len(pointsx)):
+		for i in range(5):#len(pointsx)):
+			vi = [-pointsx[i], -pointsy[i], -pointsz[i]]
+			v10 = vi
+
+			xyLength = math.sqrt(v10[0]*v10[0] + v10[1]*v10[1])
+			vecLength = math.sqrt(v10[0]*v10[0] + v10[1]*v10[1] + v10[2]*v10[2])
+			print xyLength, vecLength
+			
+			zAngle = math.acos(v10[1] / xyLength)
+			xAngle = math.acos(xyLength / vecLength)
+
+			if v10[2] < 0:
+				#print "xCorr"
+				xAngle = -xAngle
+			if v10[0] > 0:
+				#print "zCorr"
+				zAngle = -zAngle
+
+			print xAngle*180/math.pi, zAngle*180/math.pi
+
+			q = tf.transformations.quaternion_from_euler(xAngle-math.pi/2, 0, zAngle)
+			print q
+
+			'''br.sendTransform((pointsx[i], pointsy[i], pointsz[i]),
+								 (0, 0, 0, 1),
+								 rospy.Time.now(),
+								 "p" + str(i),
+								 "object")'''
 			br.sendTransform((pointsx[i], pointsy[i], pointsz[i]),
-							 (0, 0, 0, 1),
+							 (q[0], q[1], q[2], q[3]),
 							 rospy.Time.now(),
 							 "p" + str(i),
-							 "object")'''
-		br.sendTransform((v1[0], v1[1], v1[2]),
-						 (q[0], q[1], q[2], q[3]),
-						 rospy.Time.now(),
-						 "p1",
-						 "base_link")
-		br.sendTransform((v0[0], v0[1], v0[2]),
-						 (0, 0, 0, 1),
-						 rospy.Time.now(),
-						 "p0",
-						 "base_link")		
+							 "base_link")
+			'''br.sendTransform((v1[0], v1[1], v1[2]),
+							 (q[0], q[1], q[2], q[3]),
+							 rospy.Time.now(),
+							 "p1",
+							 "base_link")
+			br.sendTransform((v0[0], v0[1], v0[2]),
+							 (0, 0, 0, 1),
+							 rospy.Time.now(),
+							 "p0",
+							 "base_link")'''		
 		rate.sleep()
 
 if __name__ == '__main__':
