8e94ef9da390334e79d8e82ff1ddeadf70edfa9c
JRauer <johannes.rauer@hotmail.com>
2019-03-10 14:27:16 +0100

    Transformations work and bad positions are rejected

--- mt/src/capture_pose_calc/capture_pose_calc.py
+++ mt/src/capture_pose_calc/capture_pose_calc.py
@@ -18,24 +18,36 @@ def sample_spherical(npoints, ndim=3):
 def main(args):
 	rospy.init_node('capture_pose_calc', anonymous=True, disable_signals=True)
 
+	# Parameters for random points
 	phi = np.linspace(0, np.pi, 20)
 	theta = np.linspace(0, 2 * np.pi, 40)
 	r = 0.7
+	numPoints = 300
 	x = np.outer(np.sin(theta), np.cos(phi)) * r
 	y = np.outer(np.sin(theta), np.sin(phi)) * r
 	z = np.outer(np.cos(theta), np.ones_like(phi)) * r
+
+	# Object frame publisher for testing
+	v0 = np.array([0.3, 0.4, -0.15])
 
 	pointsx = []
 	pointsy = []
 	pointsz = []
 
-	xi, yi, zi = sample_spherical(100) * r
+	# Calculate random points
+	xi, yi, zi = sample_spherical(numPoints) * r
+
+	# Cut off points 
 	for i in range(len(xi)):
-		if zi[i] > (-r/2.5):
+		vi0 = np.array([xi[i]+v0[0], yi[i]+v0[1], zi[i]+v0[2]])
+		vi0N = math.sqrt(vi0[0]*vi0[0] + vi0[1]*vi0[1] + vi0[2]*vi0[2])
+		print vi0N
+		if zi[i] > (-r/4) and vi0N < 0.9:
 			pointsx.append(xi[i])
 			pointsy.append(yi[i])
 			pointsz.append(zi[i])
 
+	# Plot points
 	#fig, ax = plt.subplots(1, 1, subplot_kw={'projection':'3d', 'aspect':'equal'})
 	#ax.plot_wireframe(x, y, z, color='k', rstride=1, cstride=1)
 	#ax.scatter(pointsx, pointsy, pointsz, s=100, c='r', zorder=10)
@@ -43,57 +55,41 @@ def main(args):
 
 	# Init tf-broadcaster to forward pose to tf
 	br = tf.TransformBroadcaster()
-
-	# From v1 to v0 pointing
-	v0 = np.array([0, 0, 0])
-	vi = np.array([0.5, 0.8, 0.7])
 	
 	# Do at a frequency of 10 Hz
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
-		for i in range(5):#len(pointsx)):
+		for i in range(len(pointsx)):
+			# Negate Coordinates for correct orientation of vector
 			vi = [-pointsx[i], -pointsy[i], -pointsz[i]]
-			v10 = vi
 
-			xyLength = math.sqrt(v10[0]*v10[0] + v10[1]*v10[1])
-			vecLength = math.sqrt(v10[0]*v10[0] + v10[1]*v10[1] + v10[2]*v10[2])
-			print xyLength, vecLength
-			
-			zAngle = math.acos(v10[1] / xyLength)
+			# Calculate lengths and angles
+			xyLength = math.sqrt(vi[0]*vi[0] + vi[1]*vi[1])
+			vecLength = math.sqrt(vi[0]*vi[0] + vi[1]*vi[1] + vi[2]*vi[2])
+			zAngle = math.acos(vi[1] / xyLength)
 			xAngle = math.acos(xyLength / vecLength)
 
-			if v10[2] < 0:
+			# Correct angles in special cases
+			if vi[2] < 0:
 				#print "xCorr"
 				xAngle = -xAngle
-			if v10[0] > 0:
+			if vi[0] > 0:
 				#print "zCorr"
 				zAngle = -zAngle
+			#print xAngle*180/math.pi, zAngle*180/math.pi
 
-			print xAngle*180/math.pi, zAngle*180/math.pi
-
-			q = tf.transformations.quaternion_from_euler(xAngle-math.pi/2, 0, zAngle)
-			print q
-
-			'''br.sendTransform((pointsx[i], pointsy[i], pointsz[i]),
-								 (0, 0, 0, 1),
-								 rospy.Time.now(),
-								 "p" + str(i),
-								 "object")'''
+			# Calculate Quaternion representation of angles and broadcast transforms
+			q = tf.transformations.quaternion_from_euler(xAngle - math.pi/2, 0, zAngle)
 			br.sendTransform((pointsx[i], pointsy[i], pointsz[i]),
 							 (q[0], q[1], q[2], q[3]),
 							 rospy.Time.now(),
 							 "p" + str(i),
-							 "base_link")
-			'''br.sendTransform((v1[0], v1[1], v1[2]),
-							 (q[0], q[1], q[2], q[3]),
-							 rospy.Time.now(),
-							 "p1",
-							 "base_link")
+							 "object")
 			br.sendTransform((v0[0], v0[1], v0[2]),
 							 (0, 0, 0, 1),
 							 rospy.Time.now(),
-							 "p0",
-							 "base_link")'''		
+							 "object",
+							 "base_link")	
 		rate.sleep()
 
 if __name__ == '__main__':
