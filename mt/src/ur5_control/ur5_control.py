928a347ee43da29e01c1781d6a23d7e3addc096c
Johannes Rauer <johannes.rauer@hotmail.com>
2019-03-14 15:20:27 +0100

    Model changed so it can be planed with gripper and camera_planning_frame

--- mt/src/ur5_control/ur5_control.py
+++ mt/src/ur5_control/ur5_control.py
@@ -54,9 +54,11 @@ class ur5Controler():
 		group_name = "manipulator"
 		self.group = moveit_commander.MoveGroupCommander(group_name)
 		self.scene = moveit_commander.PlanningSceneInterface()
-		self.group.set_end_effector_link("gripper")
-		self.group.set_pose_reference_frame("/base_link")
-		#print self.robot.get_planning_frame()
+		self.group.set_end_effector_link("camera_planning_frame")
+		self.group.set_pose_reference_frame("/object")
+		print self.robot.get_planning_frame()
+		print self.group.get_pose_reference_frame()
+		print self.group.get_current_pose().pose
 
 		#rospy.Subscriber("/tf_objToBase", Pose, self.baseToObj_callback, queue_size=1)	# get transformation from object to base for R1-Move and planning
 		#rospy.Subscriber("/tf_objToCam", Pose, self.camToObj_callback, queue_size=1)	# get transformation from object to cam for R4-Move
