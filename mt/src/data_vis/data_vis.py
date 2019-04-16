94575748a18f67096fdb822f0759b43382c735fc
JRauer <johannes.rauer@hotmail.com>
2019-04-16 13:58:36 +0200

    Added scripts to post-process data

--- mt/src/data_vis/data_vis.py
+++ mt/src/data_vis/data_vis.py
@@ -8,15 +8,11 @@ import os
 import csv
 import json
 import re
-
-from geometry_msgs.msg import Pose
-from sensor_msgs.msg import Image
-from sensor_msgs.msg import CameraInfo
-from cv_bridge import CvBridge, CvBridgeError
+import argparse
+import glob
 import cv2
-from ur5_control import ur5_control
 
-bridge = CvBridge()
+globArgs = None
 
 def calculate_projection(points, intrinsics):
 	cuboidProj = []
@@ -30,7 +26,7 @@ def calculate_projection(points, intrinsics):
 def draw_cuboids(img, cuboidProj):
 	for i in range(len(cuboidProj)):
 		cv2.circle(img, (cuboidProj[i][0], cuboidProj[i][1]), 5, (255, 0, 0), 5)
-		print int(cuboidProj[i][0]), int(cuboidProj[i][1])
+		#print int(cuboidProj[i][0]), int(cuboidProj[i][1])
 	linePoints = [[cuboidProj[0], cuboidProj[1]],
 				  [cuboidProj[1], cuboidProj[2]],
 				  [cuboidProj[2], cuboidProj[3]],
@@ -53,13 +49,95 @@ def draw_cuboids(img, cuboidProj):
 	for i in range(8, 12):
 		cv2.line(img, (linePoints[i][0][0], linePoints[i][0][1]), (linePoints[i][1][0], linePoints[i][1][1]), (0, 255, 255), 2)
 	offset = 10
-	for i in range(len(points)):
+	for i in range(len(cuboidProj)):
 		cv2.putText(img, str(i), (cuboidProj[i][0] + offset, cuboidProj[i][1] + offset), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
 	return img
 
+def draw_boundingBox(img, boundingBox):
+	 cv2.rectangle(img, tuple(boundingBox[0]), tuple(boundingBox[1]), (255, 0, 0), 1)
+
+def conv_to_int(points):
+	points = np.asarray(points)
+	points = points.astype(int)
+
+	'''for i in range(len(points)):
+		for j in range(len(points[i])):
+			points[i][j] = int(points[i][j])'''
+	return points
+
+def visualize(root):
+	def create_data(pathToFiles):
+		'''# Open json-file and extract camera-info
+		cameraSettingsPath = pathToFiles + "/_camera_settings" + ".json"
+		if os.path.exists(cameraSettingsPath):
+			intrinsics = []
+			with open(cameraSettingsPath) as json_file:
+				data = json.load(json_file)
+				settings = data["camera_settings"]
+				intrinsics[0] = settings[0]["intrinsic_settings"]["fx"]
+				intrinsics[0] = settings[0]["intrinsic_settings"]["fy"]
+				intrinsics[0] = settings[0]["intrinsic_settings"]["cx"]
+				intrinsics[0] = settings[0]["intrinsic_settings"]["cy"]
+		else:
+			print("File not found! Images in Folder not added! " + cameraSettingsPath)
+			return'''
+
+		# For each image in the folder
+		for imgpath in glob.glob(pathToFiles+"/*.png"):
+			# If there is a png and a json file
+			if os.path.exists(imgpath) and os.path.exists(imgpath.replace("png","json")):
+				fileName = os.path.splitext(os.path.basename(imgpath))[0]
+				if globArgs.delete == False:
+					# Open json-file, extract cuboid and bounding box
+					with open(imgpath.replace("png","json")) as json_file:
+						data = json.load(json_file)
+
+						img = cv2.imread(imgpath)
+						
+						cuboidPoints = data["objects"][0]["projected_cuboid"]
+						cuboidPoints.append(data["objects"][0]["projected_cuboid_centroid"])
+						boundingBox = [data["objects"][0]["bounding_box"]["top_left"], data["objects"][0]["bounding_box"]["bottom_right"]]
+
+						if globArgs.cuboids == True or globArgs.boundingBox == False:
+							draw_cuboids(img, conv_to_int(cuboidPoints))
+						if globArgs.boundingBox == True:
+							draw_boundingBox(img, conv_to_int(boundingBox))
+
+						cv2.imwrite(pathToFiles + "/" + fileName + ".vis.png", img)
+				else:
+					os.remove(pathToFiles + "/" + fileName + ".vis.png")
+
+	def explore(pathToFiles):
+		# Return if the path is no directory
+		if not os.path.isdir(pathToFiles):
+			return
+		# Add all subfolders to variable
+		folders = [os.path.join(pathToFiles, o) for o in os.listdir(pathToFiles) 
+						if os.path.isdir(os.path.join(pathToFiles,o))]
+		# If folder has a subfolder, explore this folder (or these folders)
+		if len(folders) > 0:
+			for path_entry in folders:
+				explore(path_entry)
+		# If folder has no subfolders, create render for each image in the folder
+		else:
+			create_data(pathToFiles)
+
+	explore(root)
+
+
 def main(args):
-	img = cv2.imread("/home/johannes/git/3_0_rgb.png")
-	scaleFac = 720./400.
+	global globArgs
+	parser = argparse.ArgumentParser(description='Visualize Data')
+	parser.add_argument('Path', metavar='Path', type=str,	help='Path to folder, where recorded data is stored')
+	parser.add_argument('--delete', action="store_true", default=False, help='Delete .viz.png-Images')
+	parser.add_argument('--boundingBox', action="store_true", default=False, help='Draw bounding box')
+	parser.add_argument('--cuboids', action="store_true", default=False, help='Draw cuboid positions')
+
+	globArgs = parser.parse_args()
+
+	visualize(globArgs.Path)
+
+	'''scaleFac = 720./400.
 	imgWidth = int(round(1280./scaleFac, 0))
 	horizontalStart = int(round(imgWidth/2., 0))-200
 	img = cv2.resize(img, (imgWidth, 400), interpolation=cv2.INTER_AREA)
@@ -69,23 +147,8 @@ def main(args):
 	print len(img[0])
 	intrinsics = [925.112183, 925.379517, 647.22644, 357.068359]
 	intrinsics = [i/scaleFac for i in intrinsics]
+	intrinsics[2] = intrinsics[2]-horizontalStart'''
 
-	intrinsics[2] = intrinsics[2]-horizontalStart
-
-	points = [[0.289130581217, 0.010806052175, 0.585754207636],
-			  [0.391352787228, 0.153213179645, 0.677813029697],
-			  [0.362503297494, 0.213126918093, 0.617166100043], 
-			  [0.260281091484, 0.070719790623, 0.525107277983],
-			  [0.148562148174, 0.046003555756, 0.687394004156],
-			  [0.250784354184, 0.188410683226, 0.779452826216],
-			  [0.221934864451, 0.248324421674, 0.718805896563],
-			  [0.119712658440, 0.105917294204, 0.626747074503],
-			  [0.255532722834, 0.129565236925, 0.652280052100],
-			  ]
-	cuboidProj = calculate_projection(points, intrinsics)
-	draw_cuboids(img, cuboidProj)
-
-	cv2.imwrite("/home/johannes/git/test1.png", img)
 	return
 
 
--- mt/src/segMask_creation/segMask_creation.py
+++ mt/src/segMask_creation/segMask_creation.py
@@ -8,39 +8,98 @@ import os
 import csv
 import json
 import re
-
-from geometry_msgs.msg import Pose
-from sensor_msgs.msg import Image
-from sensor_msgs.msg import CameraInfo
-from cv_bridge import CvBridge, CvBridgeError
+import argparse
+import glob
 import cv2
-from ur5_control import ur5_control
 
-bridge = CvBridge()
+globArgs = None
+
+# Write data to json-file (formatted)
+def write_json(data, path):
+	dump = json.dumps(data, sort_keys=False, indent=4)
+	new_data = re.sub('\n +', lambda match: '\n' + '\t' * (len(match.group().strip('\n')) / 3), dump)
+	print >> open(str(path), 'w'), new_data
+
+def post_process(root):
+	def create_data(pathToFiles):
+		# Create mask and extract bounding box for each image in the folder
+		for imgpath in glob.glob(pathToFiles+"/*.render.png"):
+			inLoop = True
+			fileName = os.path.splitext(os.path.splitext(os.path.basename(imgpath))[0])[0]
+			jsonFilePath = pathToFiles + "/" + fileName + ".json"
+			# If there is a .render.png and a .json file
+			if os.path.exists(imgpath) and os.path.exists(jsonFilePath):
+				# Open json-file and read content, to write it with bounding box back
+				with open(jsonFilePath) as json_file:
+					data = json.load(json_file)
+
+					img = cv2.imread(imgpath)
+					# Get green channel
+					b, g, r = cv2.split(img)
+					# Make all pixels to 255 which are >254 because they are background, and rest to 0
+					val, img = cv2.threshold(g, 254, 255, cv2.THRESH_BINARY)
+
+					'''
+					min = 1000
+					for i in range(len(img)):
+						for j in range(len(img[0])):
+							if img[i][j] != 0 and img[i][j] != 255:
+								min = img[i][j]
+					print min'''
+
+					dilation = cv2.erode(img, np.ones((globArgs.kernel,globArgs.kernel), np.uint8), iterations = 3)
+
+					x,y,w,h = cv2.boundingRect(~dilation)
+					data["objects"][0]["bounding_box"]["top_left"] = [x, y]
+					data["objects"][0]["bounding_box"]["bottom_right"] = [x+w, y+h]
+					write_json(data, jsonFilePath)
+
+					#print x, y, w, h
+					#cv2.rectangle(dilation,(x,y),(x+w,y+h),(0,255,0),2)
+					if globArgs.create_no_masks == False:
+						cv2.imwrite(pathToFiles + "/" + fileName + ".segmentation.png", dilation)
+					if globArgs.delete_render == True:
+						os.remove(imgpath)
+		if inLoop != True:
+			print(".render.png-File not found")
+
+		if globArgs.delete_masks == True:
+			for imgpath in glob.glob(pathToFiles+"/*.segmentation.png"):
+				os.remove(imgpath)
+
+	def explore(pathToFiles):
+		# Return if the path is no directory
+		if not os.path.isdir(pathToFiles):
+			return
+		# Add all subfolders to variable
+		folders = [os.path.join(pathToFiles, o) for o in os.listdir(pathToFiles) 
+						if os.path.isdir(os.path.join(pathToFiles,o))]
+		# If folder has a subfolder, explore this folder (or these folders)
+		if len(folders) > 0:
+			for path_entry in folders:
+				explore(path_entry)
+		# If folder has no subfolders, create render for each image in the folder
+		else:
+			create_data(pathToFiles)
+
+	explore(root)
 
 def main(args):
-	img = cv2.imread("/home/johannes/git/mt/mt/src/segMask_creation/render_color.png")
-	# Get green channel
-	b, g, r = cv2.split(img)
-	# Make all pixels to 255 which are >254 because they are background, and rest to 0
-	val, img = cv2.threshold(g, 254, 255, cv2.THRESH_BINARY_INV)
+	global globArgs
+	parser = argparse.ArgumentParser(description='Create Segmentation Mask and Calculate Bounding Box')
+	parser.add_argument('Path', metavar='Path', type=str,	help='Path to folder, where recorded data is stored')
+	parser.add_argument('--kernel', action="store", type=int, default=4, help='Kernal Size for mask-creation (default: 4)')
+	parser.add_argument('--delete_render', action="store_true", default=False, help='Delete rendered images after processing')
+	parser.add_argument('--delete_masks', action="store_true", default=False, help='Delete rendered masks')
+	parser.add_argument('--create_no_masks', action="store_true", default=False, help='Create no segmentation masks')
+	globArgs = parser.parse_args()
 
-	'''
-	min = 1000
-	for i in range(len(img)):
-		for j in range(len(img[0])):
-			if img[i][j] != 0 and img[i][j] != 255:
-				min = img[i][j]
-	print min'''
+	if globArgs.kernel == 0:
+		globArgs.kernel = 1
+	path = globArgs.Path
 
-	dilation = cv2.erode(img, np.ones((5,5), np.uint8), iterations = 3)
+	post_process(path)
 
-	x,y,w,h = cv2.boundingRect(dilation)
-	print x, y, w, h
-	#cv2.rectangle(dilation,(x,y),(x+w,y+h),(0,255,0),2)
-
-	cv2.imwrite("/home/johannes/git/mt/mt/src/segMask_creation/test1.png", img)
-	cv2.imwrite("/home/johannes/git/mt/mt/src/segMask_creation/test1dilation.png", dilation)
 	return
 
 
