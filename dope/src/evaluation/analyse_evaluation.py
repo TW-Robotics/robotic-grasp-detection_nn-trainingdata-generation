c7183b33ac6c3fd54b35d3ea9b429f12e74a468d
JRauer <johannes.rauer@hotmail.com>
2019-06-12 11:14:30 +0200

    Hists for basic errors added

--- dope/src/evaluation/analyse_evaluation.py
+++ dope/src/evaluation/analyse_evaluation.py
@@ -18,6 +18,7 @@ import glob
 import argparse
 import shutil
 import re
+import matplotlib.pyplot as plt
 
 import numpy as np
 import math
@@ -56,20 +57,39 @@ class analyse_evaluation():
 		
 		for i in range(len(data["success"])):
 			self.filenamesSuccess.append(data["success"][i]["filename"])
-			self.dists3d.append(data["success"][i]["cuboid_dists3d"])
-			self.ADDCuboids.append(data["success"][i]["ADDCuboids"])
-			self.ADDModels.append(data["success"][i]["ADDModel"])
-			self.transl_errors.append(data["success"][i]["transl_error"])
-			self.rot_errors.append(data["success"][i]["rot_error"])
-			self.poses_gt.append(data["success"][i]["pose_gt"])
-			self.poses_est.append(data["success"][i]["pose_est"])
+			self.dists3d.append(data["success"][i]["cuboid_dists3d_mm"])
+			self.ADDCuboids.append(data["success"][i]["ADDCuboids_mm"])
+			self.ADDModels.append(data["success"][i]["ADDModel_mm"])
+			self.transl_errors.append(data["success"][i]["transl_error_mm"])
+			self.rot_errors.append(data["success"][i]["rot_error_deg"])
+			self.poses_gt.append(data["success"][i]["pose_gt_mm"])
+			self.poses_est.append(data["success"][i]["pose_est_mm"])
 
 		for i in range(len(data["failure"])):
 			self.filenamesFailure.append(data["failure"][i]["filename"])
 
-	def analyse(self):
-		#TODO
+	def plot_hist(self, values, title, bins, range):
+		#n, bins, patches = plt.hist(self.ADDCuboids)
+		#plt.clf() # Clear figure
+		#if bins[len(bins)-1] > 30:
+		#	print "Bigger 30"
+		n, bins, patches = plt.hist(values, bins=bins, range=range)
+		plt.xlim(range[0],range[1])
+		plt.title(title)
+		plt.xlabel("Value")
+		plt.ylabel("Count")
+		#plt.show()
 
+	def analyse(self):
+		plt.subplot(2, 2, 1)
+		self.plot_hist(self.ADDCuboids, "ADD Cuboids", 100, [0, 30])
+		plt.subplot(2, 2, 3)
+		self.plot_hist(self.ADDModels, "ADD Models", 100, [0, 30])
+		plt.subplot(2, 2, 2)
+		self.plot_hist(self.transl_errors, "Translational Errors", 100, [0, 30])
+		plt.subplot(2, 2, 4)
+		self.plot_hist(self.rot_errors, "Rotational Errors", 100, [0, 10])
+		plt.show()
 
 ###########################################################
 # VISUALISATION ###########################################
@@ -79,6 +99,6 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Analyse Evaluation')
 	parser.add_argument('Path', metavar='Path', type=str, help='Path to evaluation-file.json to analyse')
 	path = parser.parse_args().Path
-	print("Analysing evaluation '{}'...".format(Path))
+	print("Analysing evaluation '{}'...".format(path))
 
 	analyse_evaluation(path)
