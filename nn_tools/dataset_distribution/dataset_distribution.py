f7c25013cfdc7ebdf309e85b8f1306c23c986210
JRauer <johannes.rauer@hotmail.com>
2019-06-18 12:24:19 +0200

    Output more beautiful

--- nn_tools/dataset_distribution/dataset_distribution.py
+++ nn_tools/dataset_distribution/dataset_distribution.py
@@ -12,6 +12,7 @@ import random
 import argparse
 import glob
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import tf
 
 globArgs = None
@@ -22,24 +23,51 @@ yaw = []
 centerx = []
 centery = []
 
-def plot_hist(values, title, bins, range):
-	plt.hist(values, bins=bins, range=range)
+def plot_hist(values, title, bins, range, rpy=True):
+	n, bins, patches = plt.hist(values, bins=bins, range=range)
 	plt.xlim(range[0],range[1])
-	plt.title(title)
-	plt.xlabel("Value")
-	plt.ylabel("Count")
+	#plt.title(title, fontsize=15)
+	plt.xlabel(title, fontsize=12)
+	plt.ylabel("Total Count", fontsize=12)
+	if rpy == True:
+		plt.xticks(np.arange(-180, 180+45, step=45))
+		locs, labels = plt.yticks()
+		locs_n = locs[1::2]	# show only every second tick
+		plt.yticks(locs_n)
 #	plt.show()
 
 def plot_heatmap(centerx, centery):
 	#fig2 = plt.figure()
 	# Range to make empty areas same color as background
 	plt.hist2d(centerx, centery, bins=150, range=np.array([(0, 400), (0, 400)]))
-	plt.xlabel('x')
-	plt.ylabel('y')
+	plt.xlabel('x', fontsize=12)
+	plt.ylabel('y', fontsize=12)
 	cbar = plt.colorbar()
-	cbar.ax.set_ylabel('Counts')
+	cbar.ax.set_ylabel('Total Count', fontsize=12)
 	plt.axis([0, 400, 0, 400])
+	plt.xticks([0, 100, 200, 300, 400])
+	plt.yticks([0, 100, 200, 300, 400])
 #	plt.show()
+
+def plot_heatmap_3d(centerx, centery): # NOT WORKING
+	fig = plt.figure()
+	ax = fig.add_subplot(111, projection='3d')
+
+	hist, xedges, yedges = np.histogram2d(centerx, centery, bins=4, range=[[0, 4], [0, 4]])
+
+	# Construct arrays for the anchor positions of the 16 bars.
+	xpos, ypos = np.meshgrid(xedges[:-1] + 0.25, yedges[:-1] + 0.25, indexing="ij")
+	xpos = xpos.ravel()
+	ypos = ypos.ravel()
+	zpos = 0
+
+	# Construct arrays with the dimensions for the 16 bars.
+	dx = dy = 0.5 * np.ones_like(zpos)
+	dz = hist.ravel()
+
+	ax.bar3d(xpos, ypos, zpos, dx, dy, dz, zsort='average')
+
+	plt.show()
 
 def visualize(root):
 	def add_data(pathToFiles):
@@ -57,7 +85,7 @@ def visualize(root):
 					data = json.load(json_file)
 					pos = data["objects"][0]["location"]
 					ori = data["objects"][0]["quaternion_xyzw"]
-					euler = tf.transformations.euler_from_quaternion(ori, axes='rxyz')	#TODO Achsreihenfolge?!
+					euler = tf.transformations.euler_from_quaternion(ori, axes='rxyz')	#TODO Achsreihenfolge?! yxz for synth data, xyz for real data
 					#points2d = data["objects"][0]["projected_cuboid"]
 					#points3d = data["objects"][0]["cuboid"]
 					centroid_point2d = data["objects"][0]["projected_cuboid_centroid"]
@@ -72,9 +100,9 @@ def visualize(root):
 						print fileName
 						print centroid_point2d
 
-				roll.append(euler[0]*180/math.pi)
+				roll.append(euler[0]*180/math.pi+180) # TODO only for real data!
 				pitch.append(euler[1]*180/math.pi)
-				yaw.append(euler[2]*180/math.pi)
+				yaw.append(euler[2]*180/math.pi+90)
 				dist_z.append(pos[2])
 				centerx.append(centroid_point2d[0])
 				centery.append(centroid_point2d[1])
@@ -113,17 +141,28 @@ def main(args):
 	visualize(globArgs.Path)
 	print "Number of Datapoints: " + str(len(roll))
 	if len(roll) != 0:
-		plt.subplot(3, 2, 1)
-		plot_hist(roll, "Roll - Up-and-Down", 35, [-180, 180])
-		plt.subplot(3, 2, 3)
-		plot_hist(pitch, "Pitch - Camera-Turn", 35, [-180, 180])
-		plt.subplot(3, 2, 5)
-		plot_hist(yaw, "Yaw - Left-and-Right around Object", 35, [-180, 180])
-		plt.subplot(3, 2, 2)
-		plot_hist(dist_z, "Distance z", 20, [min(dist_z), max(dist_z)]) #[min(dist_z), max(dist_z)]
-		plt.subplot(3, 2, 4)
+		plt.style.use('fivethirtyeight')
+		plt.subplot(3, 1, 1)
+		plot_hist(roll, "Roll - Up-and-Down [deg]", 35, [-180, 180], rpy=True)
+		plt.subplot(3, 1, 2)
+		plot_hist(pitch, "Pitch - Camera-Turn [deg]", 35, [-180, 180], rpy=True)
+		plt.subplot(3, 1, 3)
+		plot_hist(yaw, "Yaw - Left-and-Right around Object [deg]", 35, [-180, 180], rpy=True)
+		plt.tight_layout()
+		plt.savefig(globArgs.Path + "_RPY.svg", format="svg")
+		#plt.show()
+
+		plt.subplot(1, 2, 1)
+		plot_hist(dist_z, "Distance from Camera [pixels]", 20, [min(dist_z), max(dist_z)], rpy=False)
+		plt.subplot(1, 2, 2)
+		plt.xlim(0, 400)
+		plt.ylim(0, 400)
+		plt.tight_layout()
+		#plt.axis('equal')
+		plt.gca().set_aspect('equal', adjustable='box')
 		plot_heatmap(centerx, centery)
-		plt.show()
+		plt.savefig(globArgs.Path + "_DC.svg", format="svg")
+		#plt.show()
 
 	return
 
