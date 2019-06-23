b3a5e0e0f055699bed62467d60a50f02e020f581
JRauer <johannes.rauer@hotmail.com>
2019-06-23 16:13:58 +0200

    Added probabilites

--- augmentation/aug1.py
+++ augmentation/aug1.py
@@ -9,30 +9,38 @@ from albumentations import *
 
 def strong_aug(p=0.5):
 	return Compose([
-		#GaussianBlur(blur_limit=5, p=1),
-		#MedianBlur(blur_limit=5, p=1),
-		#RandomGamma(gamma_limit=(50, 130), p=1),
-		#RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=1),
-		#JpegCompression(quality_lower=40, quality_upper=90, p=1),
-		#OneOf([
-			#IAAAdditiveGaussianNoise(scale=(2.5, 8.0), p=1)
-			#GaussNoise(var_limit=(5.0, 20.0), p=1),
-		#], p=0.2),
-		#OneOf([
-		#	CLAHE(clip_limit=3, p=1),
-		#	IAASharpen(alpha=(0.2, 0.5), lightness=(0.5, 1.0), p=1),
-		#	RandomBrightnessContrast(brightness_limit=0.35, contrast_limit=0.35, always_apply=False, p=1),
-		#	RandomBrightness(limit=0.3, p=1),
-		#	RandomContrast(limit=0.3, p=0.5),
-		#], p=0.3),
-		#HueSaturationValue(hue_shift_limit=10, sat_shift_limit=30, val_shift_limit=20, p=1),
+		# Blurs
+		OneOf([
+			GaussianBlur(blur_limit=5, p=0.5),
+			MedianBlur(blur_limit=5, p=0.5),
+		], p=0.2),
+		# Noise
+		OneOf([
+			IAAAdditiveGaussianNoise(scale=(2.5, 8.0), p=0.5),
+			GaussNoise(var_limit=(5.0, 10.0), p=0.5),
+		], p=0.2),
+		# Edges and Quality
+		OneOf([
+			JpegCompression(quality_lower=40, quality_upper=90, p=0.5),
+			IAASharpen(alpha=(0.2, 0.5), lightness=(0.5, 1.0), p=0.5),
+		], p=0.3),
+		# Brightness and Contrast
+		OneOf([
+			CLAHE(clip_limit=3, p=0.5),
+			RandomBrightnessContrast(brightness_limit=0.35, contrast_limit=0.35, always_apply=False, p=0.5),
+			RandomBrightness(limit=0.3, p=0.5),
+			RandomContrast(limit=0.3, p=0.5),
+		], p=0.7),
+		# Color
+		OneOf([
+			HueSaturationValue(hue_shift_limit=10, sat_shift_limit=30, val_shift_limit=20, p=0.5),
+			RandomGamma(gamma_limit=(50, 130), p=0.5),
+			RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=0.5),
+		], p=0.3),
 	], p=p)
 
-#image = np.ones((300, 300, 3), dtype=np.uint8)
-#mask = np.ones((300, 300), dtype=np.uint8)
-#whatever_data = "my name"
 image = cv2.imread("/home/mluser/Schreibtisch/aug_test/014732.png")
-for i in range(10):
+for i in range(30):
 	augmentation = strong_aug(p=1)
 	data = {"image": image.copy()}#, "mask": mask, "whatever_data": whatever_data, "additional": "hello"}
 	augmented = augmentation(**data)
