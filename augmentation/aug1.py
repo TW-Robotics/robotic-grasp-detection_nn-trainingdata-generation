3e2fcbc2c2deae8db089f49489de07a712604aa0
JRauer <johannes.rauer@hotmail.com>
2019-06-23 16:02:42 +0200

    Added functions and values for transformations

--- augmentation/aug1.py
+++ augmentation/aug1.py
@@ -5,25 +5,27 @@ sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 sys.path.remove('/home/mluser/catkin_ws/devel/lib/python2.7/dist-packages')
 #print(sys.path)
 import cv2
-from albumentations import (
-	HorizontalFlip, IAAPerspective, ShiftScaleRotate, CLAHE, RandomRotate90,
-	Transpose, ShiftScaleRotate, Blur, OpticalDistortion, GridDistortion, HueSaturationValue,
-	IAAAdditiveGaussianNoise, GaussNoise, MotionBlur, MedianBlur, IAAPiecewiseAffine,
-	IAASharpen, IAAEmboss, RandomBrightnessContrast, Flip, OneOf, Compose
-)
+from albumentations import *
 
 def strong_aug(p=0.5):
 	return Compose([
-		OneOf([
-			IAAAdditiveGaussianNoise(),
-			GaussNoise(),
-		], p=0.2),
-		OneOf([
-			CLAHE(clip_limit=2),
-			IAASharpen(),
-			RandomBrightnessContrast(),
-		], p=0.3),
-		HueSaturationValue(p=0.3),
+		#GaussianBlur(blur_limit=5, p=1),
+		#MedianBlur(blur_limit=5, p=1),
+		#RandomGamma(gamma_limit=(50, 130), p=1),
+		#RGBShift(r_shift_limit=15, g_shift_limit=15, b_shift_limit=15, p=1),
+		#JpegCompression(quality_lower=40, quality_upper=90, p=1),
+		#OneOf([
+			#IAAAdditiveGaussianNoise(scale=(2.5, 8.0), p=1)
+			#GaussNoise(var_limit=(5.0, 20.0), p=1),
+		#], p=0.2),
+		#OneOf([
+		#	CLAHE(clip_limit=3, p=1),
+		#	IAASharpen(alpha=(0.2, 0.5), lightness=(0.5, 1.0), p=1),
+		#	RandomBrightnessContrast(brightness_limit=0.35, contrast_limit=0.35, always_apply=False, p=1),
+		#	RandomBrightness(limit=0.3, p=1),
+		#	RandomContrast(limit=0.3, p=0.5),
+		#], p=0.3),
+		#HueSaturationValue(hue_shift_limit=10, sat_shift_limit=30, val_shift_limit=20, p=1),
 	], p=p)
 
 #image = np.ones((300, 300, 3), dtype=np.uint8)
@@ -31,7 +33,7 @@ def strong_aug(p=0.5):
 #whatever_data = "my name"
 image = cv2.imread("/home/mluser/Schreibtisch/aug_test/014732.png")
 for i in range(10):
-	augmentation = strong_aug(p=0.9)
+	augmentation = strong_aug(p=1)
 	data = {"image": image.copy()}#, "mask": mask, "whatever_data": whatever_data, "additional": "hello"}
 	augmented = augmentation(**data)
 	#image, mask, whatever_data, additional = augmented["image"], augmented["mask"], augmented["whatever_data"], augmented["additional"]
