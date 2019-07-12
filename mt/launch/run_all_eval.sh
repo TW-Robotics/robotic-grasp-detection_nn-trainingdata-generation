2bc883accb3a4716b42995b06cc350a0e7294054
Johannes Rauer <johannes.rauer@hotmail.com>
2019-07-12 11:15:13 +0200

    Added mir-upload and execution

--- mt/launch/run_all_eval.sh
+++ mt/launch/run_all_eval.sh
@@ -5,6 +5,15 @@ sleep 3
 echo "Loading parameters"
 xterm -hold -e "roslaunch mt load_config.launch" &
 
+echo "Uploading robot-model to core..."
+xterm -hold -e "roslaunch butler butler_robot.launch" &
+sleep 2
+
+echo "Launching Camera and attaching it to robot..."
+xterm -hold -e "roslaunch mt rs_aligned_depth.launch" &
+xterm -hold -e "roslaunch mt tf_publishCam.launch" &
+sleep 2
+
 echo "Connecting to UR5 and launching path-planner..."
 xterm -hold -e "roslaunch ur_modern_driver ur5_bringup.launch limited:=true robot_ip:=192.168.12.52" &
 sleep 2
@@ -13,10 +22,9 @@ sleep 2
 xterm -hold -e "roslaunch ur5_moveit_config moveit_rviz.launch config:=true" &
 sleep 3
 
-echo "Launching Camera and attaching it to robot..."
-xterm -hold -e "roslaunch mt rs_aligned_depth.launch" &
-xterm -hold -e "roslaunch mt tf_publishCam.launch" &
-sleep 2
+echo "Connecting to MiR100..."
+xterm -hold -e "roslaunch mir_driver mir.launch" &
+sleep 15
 
 echo "Launching dope and tf-helper functions"
 xterm -hold -e "roslaunch mt grasp_nodes.launch" &
